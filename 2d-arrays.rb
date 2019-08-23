72cb9aad24aa0dc8cb4cfaf1aab5dfaa828acf42
Ben Flaskrud <bflaskrud2@yahoo.com>
2019-08-23 02:32:13 +0000

    blur update

--- 2d-arrays.rb
+++ 2d-arrays.rb
@@ -1,22 +1,78 @@
 class Image
-  attr_accessor :blur
+  attr_accessor :image
 
-  def initialize(blur)
-    @blur = blur
+  def initialize(image)
+    @image = image
   end
 
   def output_image
-    @blur.each do |row|
+    @image.each do |row|
       puts row.join
     end
   end
+
+  def blur
+    ones = find_ones
+    
+    @image.each_with_index do |row, row_index|
+      row.each_with_index do |pixel, column_index|
+        # [3,1]
+        ones.each do |one_row, one_column|
+          #puts "#{row_index} - #{column_index}"
+          #puts "#{one_row} - #{one_column}"
+          ##puts "-------------------"
+
+          if column_index == one_column && row_index == one_row
+            @image[row_index-1][column_index] = 1 if row_index > 0
+            @image[row_index+1][column_index] = 1 if row_index < 6
+            @image[row_index][column_index-1] = 1 if column_index > 0
+            @image[row_index][column_index+1] = 1 if column_index < 3
+
+          end 
+        end
+        # look at the ones coordinates and compare them to the pixel
+        # if it's a match, change the surrounding pixels
+      end
+    end 
+
+    output_image
+  end
+  # iterate over the image rows and columns again
+  # compare each pixel's coordinates with the coords of the ones
+
+  def find_ones
+     # @image.each { |row| row.each { |pixel| ... } }
+    cords = []
+
+
+    @image.each_with_index do |row, row_index|
+      # row = [0, 1, 0, 0]
+      # puts "#{row_index} - ROW"
+      row.each_with_index do |pixel, column_index|
+        # pixel = 0
+        # puts "#{column_index} - COLUMN"
+        if pixel == 1
+          # save the coordinates
+          cords << [row_index, column_index]
+        end
+      end
+    end
+
+    #puts cords.inspect
+    cords
+  end
 end 
+
+# find_ones = [[3,1]]
 
 image = Image.new([
   [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [0, 0, 0, 0],
+  [1, 0, 0, 0],
   [0, 0, 0, 0]
 ])
 
-image.output_image
+ #image.output_image
+image.blur
